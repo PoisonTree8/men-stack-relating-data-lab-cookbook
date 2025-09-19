de5c910513f63f40316a12a18f180d36c421b7bd
PoisonTree8 <poison8t@gmail.com>
2025-09-19 17:55:28 +0300

    index page

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,13 +3,22 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-  res.render('foods/index.ejs');
+router.get('/', async (req, res) => {
+  try {
+const currentUser = await User.findById(req.session.user._id);
+res.locals.pantry = (currentUser && Array.isArray(currentUser.pantry)) ? currentUser.pantry : [];
+res.render('foods/index.ejs');
+} catch (error) {
+console.log(error);
+res.redirect('/');
+}
 });
+
 
 router.get('/new', (req,res) => {
     res.render('foods/new.ejs');
 })
+
 
 router.post('/', async (req,res) => {
 try {
