30149c8944fefe0deb67e90032872819b7c013bd
PoisonTree8 <poison8t@gmail.com>
2025-09-19 16:56:46 +0300

    building (create)

--- controllers/foods.js
+++ controllers/foods.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/user.js');
 
 router.get('/', (req, res) => {
@@ -12,4 +11,15 @@ router.get('/new', (req,res) => {
     res.render('foods/new.ejs');
 })
 
+router.post('/', async (req,res) => {
+try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.pantry.push(req.body);
+    await currentUser.save();
+    res.redirect('/');
+}   catch (error) {
+    console.log(error);
+    res.redirect('/');
+}
+});
 module.exports = router;
