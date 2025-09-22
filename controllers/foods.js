8101eb8bfdfdded36244746cdbf94d0dde220100
PoisonTree8 <poison8t@gmail.com>
2025-09-22 19:14:24 +0300

    building edit & update

--- controllers/foods.js
+++ controllers/foods.js
@@ -6,7 +6,8 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
   try {
 const currentUser = await User.findById(req.session.user._id);
-res.locals.pantry = (currentUser && Array.isArray(currentUser.pantry)) ? currentUser.pantry : [];
+res.locals.user = currentUser;              
+res.locals.pantry = currentUser ? currentUser.pantry : [];
 res.render('foods/index.ejs');
 } catch (error) {
 console.log(error);
@@ -18,7 +19,6 @@ res.redirect('/');
 router.get('/new', (req,res) => {
     res.render('foods/new.ejs');
 })
-
 
 router.post('/', async (req,res) => {
 try {
@@ -36,7 +36,6 @@ router.delete('/:foodId', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
     const food = currentUser.pantry.id(req.params.foodId);
-
     if (food) {
       console.log('Food Found');
       food.deleteOne();
@@ -48,9 +47,37 @@ router.delete('/:foodId', async (req, res) => {
     }
   } catch (error) {
     console.log(error);
-    res.redirect('/');
-}
+  }
 });
 
+router.get('/:foodId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    if (!food) {
+      console.log('Food not found');
+      return res.redirect(`/users/${req.params.foodId}/foods`);
+    }
+    res.locals.food = food;
+    res.render('foods/edit.ejs');
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.put('/:foodId', async (req, res) => {
+try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    if (!food) {
+      console.log('Food not found');
+      return res.redirect(`/users/${req.session.user._id}/foods`);
+    } 
+   food.name = req.body.name;
+    await currentUser.save();
+     res.redirect(`/users/${req.session.user._id}/foods`);
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
